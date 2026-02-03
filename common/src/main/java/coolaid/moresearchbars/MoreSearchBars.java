dc74d1f0c70f07c2cc26c03218c9dd9d7e083d63
CoolAid48 <atgunsel@gmail.com>
2026-02-03 17:24:56 -0500

    Initial Commit 2.0

---
+++ common/src/main/java/coolaid/moresearchbars/MoreSearchBars.java
@@ -0,0 +1,9 @@
+package coolaid.moresearchbars;
+
+public final class MoreSearchBars {
+    public static final String MOD_ID = "moresearchbars";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/coolaid/moresearchbars/neoforge/MoreSearchBarsNeoForge.java
@@ -0,0 +1,12 @@
+package coolaid.moresearchbars.neoforge;
+
+import coolaid.moresearchbars.MoreSearchBars;
+import net.neoforged.fml.common.Mod;
+
+@Mod(MoreSearchBars.MOD_ID)
+public final class MoreSearchBarsNeoForge {
+    public MoreSearchBarsNeoForge() {
+        // Run our common setup.
+        MoreSearchBars.init();
+    }
+}
